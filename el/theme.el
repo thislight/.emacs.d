ab9427c9c7cbf984381ef6add8e9a3609d477aa8
thisLight <l1589002388@gmail.com>
2021-07-02 08:45:04 +0800

    enable ligature for coding-specific fonts

--- el/theme.el
+++ el/theme.el
@@ -1,7 +1,29 @@
 (use-package spacemacs-theme
-	     :defer t
-	     :init
-	     (load-theme 'spacemacs-dark t)
-	     )
+  :defer t
+  :init
+  (load-theme 'spacemacs-dark t)
+  )
+
+;; Enable ligatures for Fira Code or others coding-specific fonts
+(use-package ligature
+  :load-path "~/.emacs.d/el/third-party/ligature.el/"
+  :config
+  ;; Copy from https://github.com/tonsky/FiraCode/wiki/Emacs-instructions#using-ligatureel
+  ;; Enable the www ligature in every possible major mode
+  (ligature-set-ligatures 't '("www"))
+
+  ;; Enable ligatures in programming modes
+  (ligature-set-ligatures 'prog-mode '("www" "**" "***" "**/" "*>" "*/" "\\\\" "\\\\\\" "{-" "::"
+                                       ":::" ":=" "!!" "!=" "!==" "-}" "----" "-->" "->" "->>"
+                                       "-<" "-<<" "-~" "#{" "#[" "##" "###" "####" "#(" "#?" "#_"
+                                       "#_(" ".-" ".=" ".." "..<" "..." "?=" "??" ";;" "/*" "/**"
+                                       "/=" "/==" "/>" "//" "///" "&&" "||" "||=" "|=" "|>" "^=" "$>"
+                                       "++" "+++" "+>" "=:=" "==" "===" "==>" "=>" "=>>" "<="
+                                       "=<<" "=/=" ">-" ">=" ">=>" ">>" ">>-" ">>=" ">>>" "<*"
+                                       "<*>" "<|" "<|>" "<$" "<$>" "<!--" "<-" "<--" "<->" "<+"
+                                       "<+>" "<=" "<==" "<=>" "<=<" "<>" "<<" "<<-" "<<=" "<<<"
+                                       "<~" "<~~" "</" "</>" "~@" "~-" "~>" "~~" "~~>" "%%"))
+  (global-ligature-mode 't)
+  )
 
 (provide 'theme)
