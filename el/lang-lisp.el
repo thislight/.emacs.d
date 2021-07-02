4323056f76d1f464c5cf9e187331a3792213339b
thisLight <l1589002388@gmail.com>
2021-07-02 08:46:23 +0800

    lisp support: add roswell as implementation & remove useless hook

--- el/lang-lisp.el
+++ el/lang-lisp.el
@@ -1,49 +1,52 @@
-
 (defun rubicon/slime-init ()
-    (progn
-      (setq slime-contribs '(slime-fancy)
-	    slime-complete-symbol-function 'slime-fuzzy-complete-symbol
-	    slime-net-coding-system 'utf-8-unix
-	    slime-lisp-implementations
-	    '((ccl ("ccl"))
-	      (clisp ("clisp" "-q"))
-	      (cmucl ("cmucl" "-quiet"))
-	      (sbcl ("sbcl" "--noinform")))
-	    slime-default-lisp 'sbcl
-	    )
-      (add-hook 'lisp-mode-hook (lambda () (run-hooks 'prelude-lisp-coding-hook)))
-      (add-hook 'slime-repl-mode-hook (lambda () (run-hooks 'prelude-interactive-lisp-coding-hook)))
-      ;; start slime automatically when we open a lisp file
-      (defun prelude-start-slime ()
-	(unless (slime-connected-p)
-	  (save-excursion (slime))))
+  (progn
+    (setq slime-contribs '(slime-fancy)
+          slime-complete-symbol-function 'slime-fuzzy-complete-symbol
+          slime-net-coding-system 'utf-8-unix
+          slime-lisp-implementations
+          '((ccl ("ccl"))
+            (clisp ("clisp" "-q"))
+            (cmucl ("cmucl" "-quiet"))
+            (sbcl ("sbcl" "--noinform"))
+            (roswell ("ros" "run")))
+          slime-default-lisp 'roswell
+	  )
+    (add-hook 'lisp-mode-hook (lambda () (run-hooks 'prelude-lisp-coding-hook)))
+    (add-hook 'slime-repl-mode-hook (lambda () (run-hooks 'prelude-interactive-lisp-coding-hook)))
+    ;; start slime automatically when we open a lisp file
+    (defun prelude-start-slime ()
+      (unless (slime-connected-p)
+        (save-excursion (slime))
+	))
 
-      (add-hook 'slime-mode-hook 'prelude-start-slime)
-      ;; Stop SLIME's REPL from grabbing DEL,
-      ;; which is annoying when backspacing over a '('
-      (defun prelude-override-slime-repl-bindings-with-paredit ()
-	(define-key slime-repl-mode-map
-	  (read-kbd-macro paredit-backward-delete-key) nil))
+    (add-hook 'slime-mode-hook 'prelude-start-slime)
 
-      (add-hook 'slime-repl-mode-hook 'prelude-override-slime-repl-bindings-with-paredit)
+    (eval-after-load "slime"
+      '(progn
+         (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol
+               slime-fuzzy-completion-in-place t
+               slime-enable-evaluate-in-emacs t
+               slime-autodoc-use-multiline-p t)
 
-      (eval-after-load "slime"
-	'(progn
-	   (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol
-		 slime-fuzzy-completion-in-place t
-		 slime-enable-evaluate-in-emacs t
-		 slime-autodoc-use-multiline-p t)
-
-	   (define-key slime-mode-map (kbd "TAB") 'slime-indent-and-complete-symbol)
-	   (define-key slime-mode-map (kbd "C-c i") 'slime-inspect)
-	   (define-key slime-mode-map (kbd "C-c C-s") 'slime-selector)))
+         (define-key slime-mode-map (kbd "TAB") 'slime-indent-and-complete-symbol)
+         (define-key slime-mode-map (kbd "C-c i") 'slime-inspect)
+         (define-key slime-mode-map (kbd "C-c C-s") 'slime-selector))
       ))
+  (defun slime-qlot-exec (directory)
+    (interactive (list (read-directory-name "Project directory: ")))
+    (slime-start :program "qlot"
+                 :program-args '("exec" "ros" "-S" "." "run")
+                 :directory directory
+                 :name 'qlot
+                 :env (list (concat "PATH=" (mapconcat 'identity exec-path ":")))
+		 )))
 
 (use-package slime
   :ensure t
   :init (rubicon/slime-init)
   :config (setq common-lisp-hyperspec-root "/usr/local/share/doc/hyperspec/HyperSpec/"
-		common-lisp-hyperspec-symbol-table (concat common-lisp-hyperspec-root "Data/Map_Sym.txt")
-		common-lisp-hyperspec-issuex-table (concat common-lisp-hyperspec-root "Data/Map_IssX.txt")))
+                common-lisp-hyperspec-symbol-table (concat common-lisp-hyperspec-root "Data/Map_Sym.txt")
+                common-lisp-hyperspec-issuex-table (concat common-lisp-hyperspec-root "Data/Map_IssX.txt")
+		))
 
 (provide 'lang-lisp)
