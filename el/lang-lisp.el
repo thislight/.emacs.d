49d13a1ee7e73d1923e4c2d73cffa2972327bf57
thisLight <l1589002388@gmail.com>
2021-07-03 15:20:16 +0800

    add lispy

--- el/lang-lisp.el
+++ el/lang-lisp.el
@@ -34,4 +34,19 @@
 (use-package sly-asdf
   :ensure t
 )
+
+;; Lispy https://github.com/abo-abo/lispy
+(use-package lispy
+  :ensure t
+  :init (progn
+          ;; enable lispy for all lisp mode
+         (add-hook 'lisp-mode-hook (lambda () (lispy-mode 1)))
+         ;; enable lispy for eval-expression
+         (defun conditionally-enable-lispy ()
+           (when (eq this-command 'eval-expression)
+                 (lispy-mode 1)
+           ))
+         (add-hook 'minibuffer-setup-hook 'conditionally-enable-lispy)
+        ))
+
 (provide 'lang-lisp)
